a7cc81810e7a8a7f873b0b1bc5ad5479ec45206a
rayannamandu <2022211510015@iesp.edu.br>
2024-05-14 11:44:59 -0300

    Aula 5

---
+++ aula_05/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import GerenciamentoAlunos from './components/gerenciamentoAlunos'
+
+const App = () => {
+  return (
+    <div><GerenciamentoAlunos /></div>
+  )
+}
+
+export default App
